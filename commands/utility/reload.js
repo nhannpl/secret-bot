45a35266dd1afa30c5208d0aac169c94d0a316d4
nhannpl <nplnhan157@gmail.com>
2024-05-28 13:48:04 -0500

    update config

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 // Replace 'YOUR_USER_ID' with your actual Discord user ID
-const ownerId = '796914999175938059';
+const  { ownerId } = require('./config.json');
 
 module.exports = {
 	category: 'utility',
